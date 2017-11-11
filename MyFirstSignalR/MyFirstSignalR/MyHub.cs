4b2e59d58c440eb44d415c43aecb1138b322d1bf
RamananSri <1058515@ucn.dk>
2017-11-11 21:09:27 +0100

    Realtime announce with message

---
+++ MyFirstSignalR/MyFirstSignalR/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyFirstSignalR
+{
+    public class MyHub : Hub
+    {
+
+        // Annoncer til alle klienter
+        public void Announce(string message)
+        {
+            Clients.All.Announce(message);
+        }
+    }
+}
---
+++ MyFirstSignalR/MyFirstSignalR/Scripts/SignalR.js
@@ -0,0 +1,17 @@
+﻿// Klienten forbinder til hub 
+$.connection.hub.start()
+.done(() => {
+    console.log("It worked")
+    // kalder announce() metoden på hub (som pusher til alle clienter)
+    $.connection.myHub.server.announce("Hello World!");
+})
+.fail(() => {
+    alert("Error!")
+});
+
+// Når announce bliver kaldt på hub kaldes denne metode (hver gang der er en der kalder announce vil der appendes en message)
+$.connection.myHub.client.announce = (message) => {
+    $("#message").append(message + "<br/>");
+}
+
+
