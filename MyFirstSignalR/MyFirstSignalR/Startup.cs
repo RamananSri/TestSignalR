e95f3d24329f717ebfff4eeac0406517e7123c20
RamananSri <1058515@ucn.dk>
2017-11-11 19:43:40 +0100

    initial commit

---
+++ MyFirstSignalR/MyFirstSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstSignalR.Startup))]
+namespace MyFirstSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
