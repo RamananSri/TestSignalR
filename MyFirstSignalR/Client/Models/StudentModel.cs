f99a0ea5ac13aa003eb0ec74e42e0c2fcd6a98e5
RamananSri <1058515@ucn.dk>
2017-11-12 23:01:10 +0100

    test MVVM project added

---
+++ MyFirstSignalR/Client/Models/StudentModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    class StudentModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string firstName;
+        private string lastName;
+
+        public string FirstName {
+            get {
+                return firstName;
+            }
+            set {
+                if(firstName != value)
+                {
+                    firstName = value;
+                    RaisePropertyChanged("Firstname");
+                    RaisePropertyChanged("Fullname");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if(lastName != value)
+                {
+                    lastName = value;
+                    RaisePropertyChanged("Lastname");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+        private void RaisePropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+    }
+}
---
+++ MyFirstSignalR/Client/ViewModels/StudentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ViewModels
+{
+    class StudentViewModel
+    {
+        public ObservableCollection<StudentModel> Students { get; set; }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<StudentModel> students = new ObservableCollection<StudentModel>();
+
+            students.Add(new StudentModel { FirstName = "Hans", LastName = "Hansen"});
+            students.Add(new StudentModel { FirstName = "Lars", LastName = "Larsen" });
+            students.Add(new StudentModel { FirstName = "Peter", LastName = "Petersen" });
+
+            Students = students;
+        }
+
+
+    }
+}
